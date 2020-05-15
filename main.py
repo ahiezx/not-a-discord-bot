04102a49349663cfff34b84b7aa6167525e60592
Ahmed <54437675+foobar429@users.noreply.github.com>
2020-05-15 21:14:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello. world")
+import discord, auth
+from time import sleep
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	for x in client.users:
+		print(x.name)
+
+client.run(auth.TOKEN)
