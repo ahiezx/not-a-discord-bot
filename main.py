15dda10a2912e4cd1f13c156b58f0633b3a4554d
Ahmed <54437675+foobar429@users.noreply.github.com>
2020-05-15 21:28:43 +0200

    rayray

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import discord, auth
+import discord, auth, config
 from time import sleep
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	for x in client.users:
-		print(x.name)
+	print("Made by {}\n{} {}".format(config.AUTHOR,client.user.name, config.VERSION))
 
 client.run(auth.TOKEN)
