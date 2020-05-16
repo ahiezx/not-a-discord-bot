5b167d14b40f3d9da5aeb3d249af5a720e6a589e
Ahmed <54437675+foobar429@users.noreply.github.com>
2020-05-16 10:33:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ bot = discord.Client()
 bot.db = MySQL(auth.HOST, auth.USER, auth.PASS, auth.PORT)
 @bot.event
 async def on_ready():
-	print("Made by {}\n{} {}".format(config.AUTHOR,bot.user.name, config.VERSION))
+	print("{} {}, Made by {}\n".format(bot.user.name, config.VERSION,config.AUTHOR))
 
 bot.run(auth.TOKEN)
