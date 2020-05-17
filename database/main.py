44c0844601da5319c67118a33043402850f321e2
xRokz <38587835+xrokz@users.noreply.github.com>
2020-05-17 22:24:57 +0300

    update db

--- database/main.py
+++ database/main.py
@@ -1,32 +1,45 @@
 import mysql.connector
 
 class MySQL:
-    def __init__(self, host, user, passwd, port):
-        self.session = None
-        self.user = user
-        self.host = host
-        self.port = port
-        self.passwd = passwd
+	def __init__(self, host, user, passwd, port, db):
+		self.session = None
+		self.user = user
+		self.host = host
+		self.port = port
+		self.passwd = passwd
+		self.db = db
 
-    def connect(self):
-        try:
-            self.session = mysql.connector.connect(
-                host = self.host,
-                user = self.user,
-                passwd = self.passwd,
-                port = self.port
-            )
-            return self.session
-        except mysql.connector.Error as err:
-            return print(err)
+	def connect(self):
+		try:
+			self.session = mysql.connector.connect(
+			host = self.host,
+			user = self.user,
+			passwd = self.passwd,
+			port = self.port,
+			database = self.db
+			)
+		except mysql.connector.Error as err:
+			self.session = mysql.connector.connect(
+			host = self.host,
+			user = self.user,
+			passwd = self.passwd,
+			port = self.port,
+			# database = self.db
+			)
+			mycursor = self.session.cursor()
+			mycursor.execute("CREATE DATABASE " + self.db)
+			self.session.commit()
+			self.connect()
 
-    def close(self):
-        self.session.close()
+		return self.session
 
-    def execute(self, command):
-        try:
-            pass
-        except:
-            pass
+	def close(self):
+		self.session.close()
+
+	def execute(self, command):
+		try:
+			pass
+		except:
+			pass
 
 # print(mydb)
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from database.main import *
 from time import sleep
 
 bot = discord.Client()
-bot.db = MySQL(auth.HOST, auth.USER, auth.PASS, auth.PORT)
+bot.db = MySQL(auth.HOST, auth.USER, auth.PASS, auth.PORT, 'discorddevelopment')
 @bot.event
 async def on_ready():
 	print("{} {} - Made by {}".format(bot.user.name, config.VERSION,config.AUTHOR))
 	connect = bot.db.connect()
+	mycursor = connect.cursor()
 	
 bot.run(auth.TOKEN)
